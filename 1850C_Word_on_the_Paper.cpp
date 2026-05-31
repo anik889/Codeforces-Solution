78b13bf76ff125ea72adb361af08c022a68927d9
NOOBmaster <84847387+anik889@users.noreply.github.com>
2026-06-01 02:16:41 +0600

    Sync Codeforces: 1850C - Word on the Paper [C++23 (GCC 14-64, msys2)]

---
+++ 1850C_Word_on_the_Paper.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+    int t ;
+    cin>>t;
+    //vector<int>a;
+    //a.reserve(64);
+    char a[8][8];
+    for(int x=0;x<t;x++){
+        for(int i=0;i<8;i++){
+            for(int j=0;j<8;j++){
+                cin>>a[i][j];
+                if(a[i][j] >= 'a' && a[i][j]<='z')
+                    cout<<a[i][j];
+ 
+            }
+        }
+        cout<<endl;
+       // t--;
+    }
+}
+int main(){
+    int n=1;
+    for(int i=0;i<n;i++)
+        solve();
+}
