1863c8916a7e22d4beca65e043314220bec1eddd
NOOBmaster <84847387+anik889@users.noreply.github.com>
2026-06-21 23:59:12 +0600

    Sync Codeforces: 2232A - Convergence [C++23 (GCC 14-64, msys2)]

--- 2232A_Convergence.cpp
+++ 2232A_Convergence.cpp
@@ -1,30 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 void solve(){
-    long long n; cin>>n;
-    vector<long long>a(n);
-    for(long long i=0;i<n;i++)    cin>>a[i];
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;i++)    cin>>a[i];
+    int c=0;
     sort(a.begin(),a.end());
-    set<long long>s;
-    long long dis,lb,ub, ans=1e18;
-    for(long long i=0;i<n;i++){
-        if(s.find(a[i])==s.end()){
-            s.insert(a[i]);
-            lb=i;
-            ub=n-(i+1);
-            dis=max(lb,ub);
-        }
-        else{
-            ub=n-(i+1)-count(a.begin()+i+1,a.end(),a[i]);
-            dis=max(lb,ub);
-        }
-        ans=min(ans,dis);
+    for(int i=0,j=n-1;i<=j;i++,j--){
+        if(a[i]==a[j] && a[i]==a[(n-1)/2])  continue;
+        c++;
     }
-    cout<<ans<<endl;
+    cout<<c<<endl;
 }
 int main(){
-    int t;
-    cin>>t;
+    int t; cin>>t;
     for(int i=0;i<t;i++)
         solve();
 }
