9a5cc1a5bd5c7ec3380d8f6bd0b17888b3e898d7
NOOBmaster <84847387+anik889@users.noreply.github.com>
2026-09-24 02:47:34 +0600

    Sync Codeforces: 1926C - Vlad and a Sum of Sum of Digits [C++23 (GCC 14-64, msys2)]

---
+++ solutions/1926C_Vlad_and_a_Sum_of_Sum_of_Digits.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+ll getDigitSum(int n){
+    ll sum=0;
+    while(n>0){
+        sum+=n%10;
+        n/=10;
+    }
+    return sum;
+}
+ll limit=2e5+5;
+vector<ll>a(limit);
+vector<ll>p(limit,0);
+void solve(){
+    ll n; cin>>n;
+    cout<<p[n]<<"\n";
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    for(ll i=0;i<limit;i++)         a[i]=getDigitSum(i+1);
+    for(ll i=1;i<limit;i++)        p[i]=p[i-1]+a[i-1];
+    int t; cin>>t;
+    for(ll i=0;i<t;i++)   solve();
+}
